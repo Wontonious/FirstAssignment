693df4cc1a65ac94b252d5a0e3b73b8b4d4fa547
Wontonious <48232238+Wontonious@users.noreply.github.com>
2019-09-17 10:36:33 -0700

    Greetings

---
+++ Lesson One/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println((90/30)+1.0);
+        System.out.println(90*50/300-11.0);
+        System.out.println(Math.sqrt(81)-5);
+        System.out.println(45.0-(82*5/10));
+        System.out.println(((500/5)+(300-100))/75.0);
+    }
+}
